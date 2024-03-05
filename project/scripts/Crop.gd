ed4f31eeedf830ec46a0b08fd5aedccd73cce6b6
javiarias <fezkip@gmail.com>
2024-03-05 15:52:29 +0100

    CROP FRAMEWORK!
    They grow! They change sprites! they also un-grow! Epic stuff!

--- project/scripts/Crop.gd
+++ project/scripts/Crop.gd
@@ -1,37 +1,61 @@
-extends Node
+extends Sprite2D
 
 
 @export var growthTime := 100.0
-@export var growthSpeed := 1.0
 @export var growthMult := 1.0
 @export var baseQuality := 1.0
 @export var qualityMult := 1.0
 
 
-@export var stages : Animation
+@export var StagesSprites : Array[Texture2D]
+var stages = StagesSprites.size()
+var growthTimeStage = growthTime / stages
 
 
+var currentStage = 1
 var growth = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	texture = StagesSprites[0]
+	stages = StagesSprites.size()
+	growthTimeStage = growthTime / stages
 	pass # Replace with function body.
 
 
 func _process(delta):
+	#update growth!
+	growth += delta * GameManager.sToMinute * growthMult
+	
 	if growth >= growthTime:
-		#Crop grown! Skip or add effects if needed?
+		# TODO: Crop grown! Skip or add effects if needed?
 		GrowthComplete(delta)
 	else:
-		#update growth!
-		growth += delta * growthSpeed * growthMult
 		
 		#update animation?
-		# TODO: UPDATE CROP ANIMATION.  GENERIC!
-	
+		if(growth >= growthTimeStage * currentStage):
+			texture = StagesSprites[currentStage]
+			currentStage += 1
+			pass
 	
 	pass
 
 
 func GrowthComplete(delta):
+	# TODO: Crop grown! Skip or add effects if needed?
+	
+	# FIXME: Temp stuff!!!
+	Harvest()
+	pass
+
+func Harvest():
+	
+	# FIXME: Temp stuff!!!
+	Reset()
+	pass
+
+func Reset():
+	texture = StagesSprites[0]
+	growth = 0
+	currentStage = 1
 	pass
